cb147745431f24dc7c45c5aa6e6b04e011a18833
Hasan <hasan.tanich@lanaco.com>
2025-02-10 12:35:17 +0100

    Add backend url

--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default ({ env }) => ({
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+      providers: {
+        google: {
+          enabled: true,
+          clientId: env("GOOGLE_CLIENT_ID"),
+          clientSecret: env("GOOGLE_CLIENT_SECRET"),
+          redirectUri: env("GOOGLE_REDIRECT_URI"),
+        },
+      },
+    },
+  },
+});
