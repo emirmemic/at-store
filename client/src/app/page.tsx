cde9e9a891a472eacf99a341505227a9052149e7
Hasan <hasan.tanich@lanaco.com>
2025-02-10 10:21:28 +0100

    Fixes for npm run build

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,51 +1,51 @@
-import qs from "qs";
-import { notFound } from "next/navigation";
+// import qs from "qs";
+// import { notFound } from "next/navigation";
 
-import { fetchAPI } from "@/lib/fetch-api";
-import { getStrapiURL } from "@/lib/utils";
-import { blockRenderer } from "@/lib/block-renderer";
-import { Block } from "@/lib/types";
+// import { fetchAPI } from "@/lib/fetch-api";
+// import { getStrapiURL } from "@/lib/utils";
+// import { blockRenderer } from "@/lib/block-renderer";
+// import { Block } from "@/lib/types";
 
-const homePageQuery = qs.stringify({
-  populate: {
-    blocks: {
-      on: {
-        "blocks.hero": {
-          populate: {
-            image: {
-              fields: ["url", "alternativeText"],
-            },
-            links: true,
-          },
-        },
-        "blocks.card-carousel": {
-          populate: {
-            cards: true,
-          },
-        },
-        "blocks.heading": true,
-      },
-    },
-  },
-});
+// const homePageQuery = qs.stringify({
+//   populate: {
+//     blocks: {
+//       on: {
+//         "blocks.hero": {
+//           populate: {
+//             image: {
+//               fields: ["url", "alternativeText"],
+//             },
+//             links: true,
+//           },
+//         },
+//         "blocks.card-carousel": {
+//           populate: {
+//             cards: true,
+//           },
+//         },
+//         "blocks.heading": true,
+//       },
+//     },
+//   },
+// });
 
-async function loader() {
-  // const authToken = process.env.STRAPI_API_TOKEN;
-  const BASE_URL = getStrapiURL();
-  const path = "/api/home-page";
-  const url = new URL(path, BASE_URL);
+// async function loader() {
+//   // const authToken = process.env.STRAPI_API_TOKEN;
+//   const BASE_URL = getStrapiURL();
+//   const path = "/api/home-page";
+//   const url = new URL(path, BASE_URL);
 
-  url.search = homePageQuery;
+//   url.search = homePageQuery;
 
-  const data = await fetchAPI(url.href, {
-    method: "GET",
-  });
+//   const data = await fetchAPI(url.href, {
+//     method: "GET",
+//   });
 
-  if (!data?.data) notFound();
+//   if (!data?.data) notFound();
 
-  const blocks = data?.data?.blocks || [];
-  return { blocks };
-}
+//   const blocks = data?.data?.blocks || [];
+//   return { blocks };
+// }
 
 export default async function HomeRoute() {
   // const data = await loader();
