0706fd08a40fcb567f3f5f82e2e98496db21a702
Hasan <hasan.tanich@lanaco.com>
2025-02-22 11:18:11 +0100

    Minor fixes

--- client/src/lib/utils/utils.ts
+++ client/src/lib/utils/utils.ts
@@ -7,8 +7,8 @@ function cn(...inputs: ClassValue[]) {
 
 function getStrapiURL() {
   return process.env.NODE_ENV === "production"
-    ? process.env.STRAPI_API_URL
-    : "http://localhost:1337";
+    ? process.env.NEXT_PUBLIC_STRAPI_API_URL
+    : process.env.STRAPI_API_URL;
 }
 
 export { cn, getStrapiURL };
