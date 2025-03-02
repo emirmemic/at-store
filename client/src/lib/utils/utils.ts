b50105daf8767d12bac4035d529530b526ed592a
Hasan <hasan.tanich@lanaco.com>
2025-03-02 10:35:07 +0100

    Fix login for development

--- client/src/lib/utils/utils.ts
+++ client/src/lib/utils/utils.ts
@@ -1,14 +1,14 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 function getStrapiURL() {
-  return process.env.NODE_ENV === "production"
+  return process.env.NODE_ENV === 'production'
     ? process.env.NEXT_PUBLIC_STRAPI_API_URL
-    : process.env.STRAPI_API_URL;
+    : 'http://localhost:1337';
 }
 
 export { cn, getStrapiURL };
