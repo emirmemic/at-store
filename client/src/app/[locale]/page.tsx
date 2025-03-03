1bc68381f91efe5e778d350a927183075e99cc80
Hasan <hasan.tanich@lanaco.com>
2025-03-03 02:02:27 +0100

    Clean homepage

--- client/src/app/[locale]/page.tsx
+++ client/src/app/[locale]/page.tsx
@@ -51,24 +51,7 @@ export default async function HomeRoute() {
   // const data = await loader();
   // const blocks = data.blocks;
   return (
-    <>
-      <div className="display">Display</div>
-      <div className="heading-1">Heading 1</div>
-      <div className="heading-2">Heading 2</div>
-      <div className="heading-3">Heading 3</div>
-      <div className="heading-4">Heading 4</div>
-      <div className="heading-5">Heading 5</div>
-      <div className="title">Title 1</div>
-      <div className="paragraph-1">Paragraph 1</div>
-      <div className="paragraph-2">Paragraph 2</div>
-      <div className="paragraph-3">Paragraph 3</div>
-      <div className="paragraph-4">Paragraph 4</div>
-      <div className="paragraph-5">Paragraph 5</div>
-      <div className="paragraph-6">Paragraph 6</div>
-      <div className="navigation">Navigation Text</div>
-      <div className="button-1">Button 1</div>
-      <div className="button-2">Button 2</div>
-    </>
+    <h1 className="display">Home page</h1>
     // <div>
     //   {blocks.map((block: Block, index: number) => {
     //     return blockRenderer(block, index);
