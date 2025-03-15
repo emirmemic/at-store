1d6f0c50693504b7dafeb508ec4744b9901dab54
makarooona <ibrahimtanicho@gmail.com>
2025-03-16 00:49:58 +0100

    fixed requested changes

--- client/src/app/[locale]/(static-pages)/components/section.tsx
+++ client/src/app/[locale]/(static-pages)/components/section.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { cn } from '@/lib/utils/utils';
+
 export interface SectionProps {
   title: string;
   listItems: string[];
@@ -15,15 +17,16 @@ export default function Section({
 }: Readonly<SectionProps>) {
   return (
     <section>
-      <h2 className="heading-4 md:heading-3">
-        {sectionNumber}
-        {'. '}
-        {title}
-      </h2>
+      <h2 className="heading-4 md:heading-3">{`${sectionNumber}. ${title}`}</h2>
       {subTitle && (
         <p className="pl-9 heading-5 md:pl-14 md:heading-4"> {subTitle} </p>
       )}
-      <ul className="list-inside list-disc pl-12 paragraph-2 md:pl-[74px] md:paragraph-1">
+      <ul
+        className={cn(
+          'list-inside list-disc paragraph-2 md:paragraph-1',
+          subTitle ? 'pl-12 md:pl-[74px]' : 'pl-9 md:pl-14'
+        )}
+      >
         {listItems.map((list, index) => (
           <li key={index}>{list}</li>
         ))}
