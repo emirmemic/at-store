d8c90adc7385013393dbf9317f1e1bd972f0c7b8
Hasan Tanich <hasantanich@gmail.com>
2025-03-18 00:22:55 +0100

    Blur import remove

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,8 +1,7 @@
-import Blur from './blur';
 import { Footer } from './footer';
 import { FormField } from './forms';
 import GlobalRenderer from './global-renderer';
 import { Navbar } from './nav-bar';
 import { StrapiImage } from './strapi-image';
 
-export { Blur, StrapiImage, Footer, Navbar, FormField, GlobalRenderer };
+export { StrapiImage, Footer, Navbar, FormField, GlobalRenderer };
