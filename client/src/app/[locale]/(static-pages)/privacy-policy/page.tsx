bd963daaa35f2bc2d3ed7304eb5c181da7e46200
makarooona <ibrahimtanicho@gmail.com>
2025-03-19 12:12:51 +0100

    Privacy Policy Page

---
+++ client/src/app/[locale]/(static-pages)/privacy-policy/page.tsx
@@ -0,0 +1,115 @@
+import { useTranslations } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
+
+import Section, {
+  SectionProps,
+} from '@/app/[locale]/(static-pages)/components/section';
+
+interface GenerateMetadataParams {
+  params: Promise<{ locale: string }>;
+}
+export async function generateMetadata({ params }: GenerateMetadataParams) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: 'metaData' });
+  return {
+    title: t('privacyPolicy.title'),
+    description: t('privacyPolicy.description'),
+    openGraph: {
+      title: t('privacyPolicy.title'),
+      description: t('privacyPolicy.description'),
+    },
+  };
+}
+
+export default function PrivacyPolicy() {
+  const t = useTranslations('privacyPolicyPage');
+  const contactT = useTranslations('contact');
+  const sections: SectionProps[] = [
+    {
+      title: t('item1.title'),
+      listItems: [
+        [{ text: t('item1.listItem') }],
+        [{ text: t('item1.listItem2') }],
+        [{ text: t('item1.listItem3') }],
+      ],
+      sectionNumber: 1,
+    },
+    {
+      title: t('item2.title'),
+      listItems: [
+        [{ text: t('item2.listItem1') }],
+        [{ text: t('item2.listItem2') }],
+        [{ text: t('item2.listItem3') }],
+        [{ text: t('item2.listItem4') }],
+      ],
+      sectionNumber: 2,
+    },
+    {
+      title: t('item3.title'),
+      listItems: [
+        [{ text: t('item3.listItem1') }],
+        [{ text: t('item3.listItem2') }],
+      ],
+      sectionNumber: 3,
+    },
+    {
+      title: t('item4.title'),
+      listItems: [[{ text: t('item4.listItem1') }]],
+      sectionNumber: 4,
+    },
+    {
+      title: t('item5.title'),
+      listItems: [
+        [{ text: t('item5.listItem1') }],
+        [{ text: t('item5.listItem2') }],
+        [{ text: t('item5.listItem3') }],
+        [
+          { text: t('item5.listItem4') },
+          {
+            text: contactT('atStoreEmail'),
+            path: contactT('atStoreEmailLink'),
+          },
+        ],
+      ],
+      sectionNumber: 5,
+    },
+    {
+      title: t('item6.title'),
+      listItems: [[{ text: t('item6.listItem1') }]],
+      sectionNumber: 6,
+    },
+    {
+      title: t('item7.title'),
+      listItems: [[{ text: t('item7.listItem1') }]],
+      sectionNumber: 7,
+    },
+    {
+      title: t('item8.title'),
+      listItems: [
+        [
+          { text: t('item8.listItem1') },
+          {
+            text: contactT('atStoreEmail'),
+            path: contactT('atStoreEmailLink'),
+          },
+        ],
+      ],
+      sectionNumber: 8,
+    },
+  ];
+
+  return (
+    <div className="w-full py-10 pt-12 container-max-width md:py-[60px]">
+      <h1 className="mb-9 text-center heading-1 md:mb-16">{t('title')}</h1>
+      <p className="mb-5 paragraph-2 md:paragraph-1">
+        <span className="heading-5 md:heading-4">{t('paragraphHeading')}</span>
+        {`${' '}${' '}${t('paragraph')}`}
+      </p>
+      <div className="flex flex-col gap-5">
+        {sections.map((section) => (
+          <Section key={section.sectionNumber} {...section} />
+        ))}
+      </div>
+    </div>
+  );
+}
