b29799ff6ae9b9609004b9162c399edec43fd02b
makarooona <ibrahimtanicho@gmail.com>
2025-03-19 11:37:16 +0100

    Order Cancellation page

---
+++ client/src/app/[locale]/(static-pages)/order-cancellation/page.tsx
@@ -0,0 +1,79 @@
+import { useTranslations } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
+
+import Section, {
+  SectionProps,
+} from '@/app/[locale]/(static-pages)/components/section';
+
+interface GenerateMetadataParams {
+  params: Promise<{ locale: string }>;
+}
+export async function generateMetadata({ params }: GenerateMetadataParams) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: 'metaData' });
+  return {
+    title: t('orderCancellation.title'),
+    description: t('orderCancellation.description'),
+    openGraph: {
+      title: t('orderCancellation.title'),
+      description: t('orderCancellation.description'),
+    },
+  };
+}
+
+export default function OrderCancellation() {
+  const t = useTranslations('orderCancellationPage');
+  const contactT = useTranslations('contact');
+  const sections: SectionProps[] = [
+    {
+      title: t('item1.title'),
+      listItems: [
+        [{ text: t('item1.listItem1') }],
+        [{ text: t('item1.listItem2') }],
+        [{ text: t('item1.listItem3') }],
+      ],
+      subTitle: t('item1.subTitle'),
+      sectionNumber: 1,
+    },
+    {
+      title: t('item2.title'),
+      listItems: [
+        [{ text: t('item2.listItem1') }],
+        [
+          {
+            text: contactT('telephone'),
+            path: contactT('telephoneLink'),
+          },
+        ],
+        [{ text: contactT('orderEmail'), path: contactT('orderEmailLink') }],
+      ],
+      sectionNumber: 2,
+    },
+    {
+      title: t('item3.title'),
+      listItems: [[{ text: t('item3.listItem1') }]],
+      sectionNumber: 3,
+    },
+    {
+      title: t('item4.title'),
+      listItems: [[{ text: t('item4.listItem1') }]],
+      sectionNumber: 4,
+    },
+    {
+      title: t('item5.title'),
+      listItems: [[{ text: t('item5.listItem1') }]],
+      sectionNumber: 5,
+    },
+  ];
+
+  return (
+    <div className="w-full py-10 pt-12 container-max-width md:py-[60px]">
+      <h1 className="mb-9 text-center heading-1 md:mb-16">{t('title')}</h1>
+      <div className="flex flex-col gap-5">
+        {sections.map((section) => (
+          <Section key={section.sectionNumber} {...section} />
+        ))}
+      </div>
+    </div>
+  );
+}
