8e397ff4b70b959e1aec9f8d30f45f523c700094
makarooona <ibrahimtanicho@gmail.com>
2025-03-29 11:29:43 +0100

    fixed images index after rebase

--- client/src/assets/images/index.ts
+++ client/src/assets/images/index.ts
@@ -1,8 +1,4 @@
 export { default as monri } from './monri.jpg';
-export { default as macbookAirM3 } from './macbook_air_m3.png';
-export { default as iphone13 } from './iphone_13.png';
-export { default as airpodsPro2 } from './airpods_pro_2.png';
-export { default as iphone14 } from './iphone_14.png';
 export { default as atStoreAlta } from './at-store-alta.png';
 export { default as atStoreDelta } from './at-store-delta.png';
 export { default as atStoreScc } from './at-store-scc.png';
