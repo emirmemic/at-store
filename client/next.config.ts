c6670cf0334b2642f93de0bcd88c4ef202ce6aa2
nurdanokcu <nurokcudan@gmail.com>
2025-04-27 23:01:39 +0200

    next config updated for images

--- client/next.config.ts
+++ client/next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'at-server.rovoit.com',
+        hostname: process.env.NEXT_PUBLIC_STRAPI_HOSTNAME ?? 'admin.atstore.ba',
       },
     ],
   },
