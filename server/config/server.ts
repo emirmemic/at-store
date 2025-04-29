4ad7126acbf8107fd971ebc1f58c964947b0710d
nurdanokcu <nurokcudan@gmail.com>
2025-04-29 18:07:16 +0200

    config server.ts is adjusted for url to work

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,11 +1,9 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env(
-    env("NODE_ENV") === "production"
-      ? env("PUBLIC_URL")
-      : "http://localhost:1337"
-  ),
+  url: env("NODE_ENV") === "production"
+  ? env("PUBLIC_URL")
+  : "http://localhost:1337/",
   proxy: {
     koa: true
   },
