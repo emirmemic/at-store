9ee58f9f9f02909e8a9e26a16de7b63ebdf03206
nurdanokcu <nurokcudan@gmail.com>
2025-04-30 15:30:22 +0200

    added documentation for buildFilter function

--- client/src/app/[locale]/(dynamic-pages)/accessories/actions/build-filters.ts
+++ client/src/app/[locale]/(dynamic-pages)/accessories/actions/build-filters.ts
@@ -4,7 +4,31 @@ interface BuildFiltersOptions {
     [key: string]: string[]; // key is field name (color, brand, material), value is array of selected filters
   };
 }
-
+/**
+ * Builds a set of filters for querying data based on category and selected filter values.
+ *
+ * @param options - The options for building filters.
+ * @param options.categoryName - The name of the category to filter by.
+ * @param options.filters - An object where keys are field names (e.g., color, brand)
+ * and values are arrays of selected filter values.
+ * @returns A record containing the constructed filters.
+ *
+ * @example
+ * const filters = buildFilters({
+ *   categoryName: 'Accessories',
+ *   filters: {
+ *     color: ['red', 'blue'],
+ *     brand: ['Apple', 'Beats'],
+ *   },
+ * });
+ * console.log(filters);
+ * // Output:
+ * // {
+ * //   category: { name: { $eq: 'Accessories' } },
+ * //   color: { name: { $in: ['red', 'blue'] } },
+ * //   brand: { name: { $in: ['Apple', 'Beats'] } },
+ * // }
+ */
 export function buildFilters({
   categoryName,
   filters = {},
