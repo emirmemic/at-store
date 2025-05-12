12246ee53996bdbacbe7de7c1afc08ca7ac75428
nurdanokcu <nurokcudan@gmail.com>
2025-05-12 15:05:21 +0200

    documentation update

--- server/src/api/product/services/related-products.ts
+++ server/src/api/product/services/related-products.ts
@@ -1,13 +1,10 @@
 export default ({ strapi }) => ({
   /**
-   * Fetches product variants by slug, gets all the products of the same type and
-   * organizes them by their attributes(color, memory etc.) and populates available options.
-   * @param {string} slug - The slug of the product.
-   * @returns {Promise<any>} - The product variants with unified response format and
-   * available options.
+   * Fetches related products based on a product type ID, gets all the products of the same type
+   * @param {string} typeId - The ID of the product type.
+   * @returns {Promise<any>} - The related products.
    */
   async findRelatedProducts(typeId: string) {
-    // Fetch all products of the same type and organize them by their attributes and populate available options
     const products = await strapi.documents('api::product.product').findMany({
       filters: {
         deviceCompatibility: {
