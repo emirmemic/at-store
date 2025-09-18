cd4ea8886243638e46808102999021f78b145dbb
Haris Hanjalic <hanjalic@proton.me>
2025-09-18 13:47:51 +0200

    Feature: Mikrofin Fix

--- client/src/app/[locale]/(dynamic-pages)/mikrofin-invoice/page.tsx
+++ client/src/app/[locale]/(dynamic-pages)/mikrofin-invoice/page.tsx
@@ -35,7 +35,7 @@ export default function Page() {
           <img
             alt="Mikrofin"
             className="mx-auto mb-6 h-20 w-auto md:h-24"
-            src={`${process.env.NEXT_PUBLIC_FRONTEND_URL}assets/images/Mikrofin-logo_1.jpg`}
+            src={`${process.env.NEXT_PUBLIC_FRONTEND_URL}/assets/images/Mikrofin-logo_1.jpg`}
           />
           <h1 className="mb-4 text-4xl font-bold text-white md:text-5xl">
             Finansiranje uz Mikrofin
@@ -58,7 +58,7 @@ export default function Page() {
             <div className="flex flex-col items-center rounded-3xl border border-gray-200 bg-white/20 p-8 shadow-md backdrop-blur-xl transition-all duration-500 hover:shadow-xl">
               <img
                 className="mx-auto mb-4 h-14"
-                src={`${process.env.NEXT_PUBLIC_FRONTEND_URL}assets/images/rok-otplate.svg`}
+                src={`${process.env.NEXT_PUBLIC_FRONTEND_URL}/assets/images/rok-otplate.svg`}
                 alt="Rok otplate"
               />
               <h3 className="mb-2 text-xl font-semibold text-gray-900">
@@ -71,7 +71,7 @@ export default function Page() {
             <div className="flex flex-col items-center rounded-3xl border border-gray-200 bg-white/20 p-8 shadow-md backdrop-blur-xl transition-all duration-500 hover:shadow-xl">
               <img
                 className="mx-auto mb-4 h-14"
-                src={`${process.env.NEXT_PUBLIC_FRONTEND_URL}assets/images/iznos.svg`}
+                src={`${process.env.NEXT_PUBLIC_FRONTEND_URL}/assets/images/iznos.svg`}
                 alt="Brzo odobrenje"
               />
               <h3 className="mb-2 text-xl font-semibold text-gray-900">
@@ -84,7 +84,7 @@ export default function Page() {
             <div className="flex flex-col items-center rounded-3xl border border-gray-200 bg-white/20 p-8 shadow-md backdrop-blur-xl transition-all duration-500 hover:shadow-xl">
               <img
                 className="mx-auto mb-4 h-14"
-                src={`${process.env.NEXT_PUBLIC_FRONTEND_URL}assets/images/online.svg`}
+                src={`${process.env.NEXT_PUBLIC_FRONTEND_URL}/assets/images/online.svg`}
                 alt="Bez papira"
               />
               <h3 className="mb-2 text-xl font-semibold text-gray-900">
