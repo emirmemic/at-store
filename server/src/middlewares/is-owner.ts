216b76f9336c23fe2b9bbda1f21fbf0a7ebcb141
Haris Hanjalic <hanjalic@proton.me>
2025-10-10 15:07:45 +0200

    Feature: Dashboard preparations

--- server/src/middlewares/is-owner.ts
+++ server/src/middlewares/is-owner.ts
@@ -1,19 +1,22 @@
 export default () => {
   return async (ctx, next) => {
     const { user } = ctx.state;
+    const isApiToken = ctx.state.auth?.strategy === 'api-token';
 
-    if (!user) {
+    if (!user && !isApiToken) {
       return ctx.unauthorized('You are not authorized to access this resource');
     }
 
-    // Add a filter to only show the current user's resources
-    if (ctx.request.query.filters) {
-      ctx.request.query.filters = {
-        ...ctx.request.query.filters,
-        user: user.id,
-      };
-    } else {
-      ctx.request.query.filters = { user: user.id };
+    if (user) {
+      // Restrict REST responses to the signed-in user's orders
+      if (ctx.request.query.filters) {
+        ctx.request.query.filters = {
+          ...ctx.request.query.filters,
+          user: user.id,
+        };
+      } else {
+        ctx.request.query.filters = { user: user.id };
+      }
     }
 
     return next();
