a5deaed2df73e850ce032a457d774695556172b2
Haris Hanjalic <hanjalic@proton.me>
2025-10-10 15:03:03 +0200

    Feature: Dashboard preparations

--- server/src/api/order/routes/order.ts
+++ server/src/api/order/routes/order.ts
@@ -7,7 +7,7 @@ import { factories } from '@strapi/strapi';
 export default factories.createCoreRouter('api::order.order', {
   config: {
     find: {
-      // middlewares: ['global::is-owner'],
+      middlewares: ['global::is-owner'],
     },
   },
 });
